0f08ad36eeee50ad57a599e1b0d3d73357c7c380
Şahin Baran Eşsiz <53182048+SahinBaranEssiz@users.noreply.github.com>
2020-12-14 18:50:35 +0300

    Update Program.cs

--- Sample_10/Sample_10/Program.cs
+++ Sample_10/Sample_10/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using
 
 namespace Sample_10
 {
@@ -6,8 +7,35 @@ namespace Sample_10
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            // Create an in-process speech recognizer for the en-US locale.
+            using (SpeechRecognitionEngine recognizer = new SpeechRecognitionEngine(new System.Globalization.CultureInfo("en-US")))
+            {
+                // Create and load a dictation grammar.
+                recognizer.LoadGrammar(new DictationGrammar());
+
+                // Add a handler for the speech recognized event.
+                recognizer.SpeechRecognized +=
+                  new EventHandler<SpeechRecognizedEventArgs>(recognizer_SpeechRecognized);
+
+                // Configure input to the speech recognizer.
+                recognizer.SetInputToDefaultAudioDevice();
+
+                // Start asynchronous, continuous speech recognition.
+                recognizer.RecognizeAsync(RecognizeMode.Multiple);
+
+                // Keep the console window open.
+                while (true)
+                {
+                    Console.ReadLine();
+                }
+            }
+            // Handle the SpeechRecognized event.
+            static void recognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+            {
+                Console.WriteLine("Recognized text: " + e.Result.Text);
+            }
         }
+
     }
+    
 }
