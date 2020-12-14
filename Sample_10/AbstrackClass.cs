1727a1bcbc6f26fcb1320a0af421829a354bef64
Şahin Baran Eşsiz <53182048+SahinBaranEssiz@users.noreply.github.com>
2020-12-14 18:50:32 +0300

    Create AbstrackClass.cs

---
+++ Sample_10/AbstrackClass.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Class1
+{
+	abstract class Deneme
+    {
+		public virtual void set()
+        {
+
+        }
+    }
+    class Guru99Tutorial : Deneme
+    {
+        protected int DenemeID;
+        protected string DenemeName;
+
+        public void SetTutorial(int pID, string pName)
+        {
+            DenemeID = pID;
+            DenemeName = pName;
+        }
+
+        public String GetTutorial()
+        {
+            return DenemeName;
+        }
+
+        static void Main(string[] args)
+        {
+            Guru99Deneme pTutor = new Guru99Deneme();
+
+            pTutor.SetDeneme(1, ".Net");
+
+            Console.WriteLine(pTutor.GetDeneme());
+
+            Console.ReadKey();
+        }
+    }
+}
